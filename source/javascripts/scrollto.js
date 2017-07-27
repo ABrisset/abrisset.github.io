10de3962520ce8ad1e826cc6a4c25a4738101f18
Antoine Brisset <ant.brisset@gmail.com>
2017-07-27 14:50:37 +0200

    add mentions

--- source/javascripts/scrollto.js
+++ source/javascripts/scrollto.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.scroll').click( function() { // Onclick
     var page = $(this).attr('href'); // Target page
-    var speed = 250; // Animation duration (ms)
+    var speed = 500; // Animation duration (ms)
     $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
     return false;
   });
